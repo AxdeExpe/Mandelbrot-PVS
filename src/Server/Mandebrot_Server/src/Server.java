f63f58cec93afbd7a2c59540f664526ec38584a7
Kev <103242766+AxdeExpe@users.noreply.github.com>
2023-05-23 19:47:14 +0200

    delete Server.java

--- src/Server/Mandebrot_Server/src/Server.java
+++ src/Server/Mandebrot_Server/src/Server.java
@@ -1,19 +1,1 @@
-public class Server extends Thread{
 
-    public void run(){
-        while(true){
-            try{
-                Thread.sleep(1000);
-                System.out.println("sheesh");
-            }
-            catch(InterruptedException e){
-
-                //create new Client
-                System.out.println("HALOOOOOOOOOOOOOOOOoo");
-
-                break;
-            }
-        }
-    }
-
-}
